8f2404b128f69e65476a4c7785c751ad9cd102dc
versx <versx@ver.sx>
2023-10-06 11:20:04 -0700

    even for styled table rows

--- src/components/StyledTableRow.tsx
+++ src/components/StyledTableRow.tsx
@@ -2,7 +2,7 @@ import { TableRow } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 export const StyledTableRow = styled(TableRow)(({ theme }: any) => ({
-  '&:nth-of-type(odd)': {
+  '&:nth-of-type(even)': {
     backgroundColor: theme.palette.action.hover,
   },
   // hide last border
